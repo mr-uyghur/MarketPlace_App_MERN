9d342121e98b7199e987d8b14a30ba8e67302963
alimran halmamat <mr.uygur1996@gmail.com>
2021-04-03 17:40:55 -0700

    added front add components with router dependecy

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 //import componenets//pages inside my App
 //based on the path show each componenets/page using react-router
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
 import Home from './booking/Home'
 import Login from './auth/Login'
 import Register from './auth/Register'
@@ -7,9 +9,16 @@ import Register from './auth/Register'
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    //wrap my componenets inside BrowserRouter
+    <BrowserRouter>
+
+      <Switch>
+        <Route exact path = "/" component={Home}/>
+        <Route exact path = "/login" component={Login}/>
+        <Route exact path = "/register" component={Register}/>
+      </Switch>    
+      
+    </BrowserRouter>
   );
 }
 
