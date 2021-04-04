ded4c0dc0ab54d1b8a5a0a90117a77da76eea409
alimran halmamat <mr.uygur1996@gmail.com>
2021-04-03 17:29:50 -0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+//import componenets//pages inside my App
+//based on the path show each componenets/page using react-router
+import Home from './booking/Home'
+import Login from './auth/Login'
+import Register from './auth/Register'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/auth/Register.js
@@ -0,0 +1,9 @@
+const Register = () =>{
+    return (
+        <div className="container-fluid h1 p-5 text-center">
+            Register page
+            </div>
+    )
+}
+
+export default Register;
