9ecd57230d0d143b02667c90cd3234b6e219dcbb
alimran halmamat <mr.uygur1996@gmail.com>
2021-04-16 00:04:39 -0700

    redux state acesssing

--- client/src/booking/Home.js
+++ client/src/booking/Home.js
@@ -1,7 +1,12 @@
+import {userSelector, useSelector, useStore} from 'react-redux'
+
 const Home = () =>{
+    //useSelector takes a function and returns a state
+const { user } = useSelector((state) =>({...state}))
     return (
         <div className="container-fluid h1 p-5 text-center">
             Home page
+            {JSON.stringify(user)}
             </div>
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -4,24 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 //1, import from react redux and redux
-import {createStore, combineReducers} from 'redux'
+import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 import {composeWithDevTools} from 'redux-devtools-extension'
 //2, create user reducer function and
-const authReducer = (state = {}, action) =>{ //
-  switch(action.type) {
-    case "LOGGED_IN_USER":
-      return {...state, ...action.payload}
-    case "LOGGED_OUT":
-      return action.payload
-    default:
-      return state
-  }
-}
 //3. combine multiple reducers
-const rootReducer = combineReducers({
-  user:authReducer,
-})
+import rootReducer from './reducers'
 //4.creat redux store 
 const store = createStore(rootReducer, composeWithDevTools())
 //5. provice redux store to the entire app
