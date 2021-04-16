357b4913b01076d31b0371752e5deba8d86b7eba
alimran halmamat <mr.uygur1996@gmail.com>
2021-04-15 23:45:50 -0700

    Craeted redux store for all components

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
 //import componenets//pages inside my App
 //based on the path show each componenets/page using react-router
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
-
+// import Link from 'react-router-dom'
 import Home from './booking/Home'
 import Login from './auth/Login'
 import Register from './auth/Register'
+import TopNav from './componenets/TopNav'
 
 
 function App() {
   return (
     //wrap my componenets inside BrowserRouter
     <BrowserRouter>
-
+    {/* Top navigation/menu component will always be on top */}
+      <TopNav/>
       <Switch>
         <Route exact path = "/" component={Home}/>
         <Route exact path = "/login" component={Login}/>
         <Route exact path = "/register" component={Register}/>
       </Switch>    
-      
+
     </BrowserRouter>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//1, import from react redux and redux
+import {createStore, combineReducers} from 'redux'
+import {Provider} from 'react-redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+//2, create user reducer function and
+const authReducer = (state = {}, action) =>{ //
+  switch(action.type) {
+    case "LOGGED_IN_USER":
+      return {...state, ...action.payload}
+    case "LOGGED_OUT":
+      return action.payload
+    default:
+      return state
+  }
+}
+//3. combine multiple reducers
+const rootReducer = combineReducers({
+  user:authReducer,
+})
+//4.creat redux store 
+const store = createStore(rootReducer, composeWithDevTools())
+//5. provice redux store to the entire app
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store = {store}> 
     <App />
+    </Provider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
