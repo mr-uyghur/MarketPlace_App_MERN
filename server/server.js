73ecd2d5f28953fd0da298fbb1591cbf68d249a5
alimran halmamat <mr.uygur1996@gmail.com>
2021-04-17 22:47:36 -0700

    added morgan dev env

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 
 import express from 'express'
+const morgan = require("morgan")
 //code for importing our routes easier
 import {readdirSync} from 'fs'
+require('dotenv').config() //execude config() function of dotenv
 
 const app = express();
 //app."get" is really coming from the express server 
 //since express() is stored in app var
-
+//morgan middleware
+app.use(morgan("dev")) // we want to run morgan in development mode
 //route middleware with express' use method
 // with this code we wanna make sure we read everything in the routes to make things easy
 // we map through each files in routes folder
@@ -14,5 +17,6 @@ const app = express();
 readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)))
 
 
+const port = process.env.PORT || 8000
 
-app.listen(8000, () => console.log(`Server is running on port 8000`))
+app.listen(port, () => console.log(`Server is running on port ${port}`))
