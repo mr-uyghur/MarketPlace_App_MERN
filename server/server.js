b081a0516155ca37011f2b2cf5519f6bb000559a
alimran halmamat <mr.uygur1996@gmail.com>
2021-04-17 23:44:47 -0700

    server connected to mogoDB

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 
 import express from 'express'
+import cors from 'cors'
+import mogoose from 'mongoose'
 const morgan = require("morgan")
 //code for importing our routes easier
 import {readdirSync} from 'fs'
 require('dotenv').config() //execude config() function of dotenv
 
 const app = express();
-//app."get" is really coming from the express server 
-//since express() is stored in app var
+
+//db connection
+mogoose.connect(process.env.DATABASE,{
+    useNewUrlParser:true,
+    useFindAndModify:false,
+    useUnifiedTopology:true,
+    useCreateIndex:true,
+})
+.then(() =>console.log('DB connected'))
+.catch(err => console.log('Error ----',err))
+
+
+//middlewares
+app.use(cors())
+
 //morgan middleware
 app.use(morgan("dev")) // we want to run morgan in development mode
 //route middleware with express' use method
@@ -20,3 +35,5 @@ readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)))
 const port = process.env.PORT || 8000
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
+
+// OKSSvMi7bsq336xU
